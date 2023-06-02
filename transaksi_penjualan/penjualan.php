d60ae1130b83a79e7b32375b8a613cad3cda6a66
Maulana775 <anakbudiman01@gmail.com>
2023-06-03 04:37:11 +0700

    maulana

--- transaksi_penjualan/penjualan.php
+++ transaksi_penjualan/penjualan.php
@@ -1,90 +1,71 @@
+<?php
+require_once "../koneksi.php";
+
+$sql = "SELECT * FROM transaksi_penjualan";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html>
 <head>
-  <title>transaksi penjualan</title>
-  <style>
-        table {
-            border-collapse: collapse;
-            width: 70%;
-            margin: 0 auto;
-            font-family: Arial, sans-serif;
-        }
-
-        th, td {
-            text-align: left;
-            padding: 8px;
-        }
-
-        th {
-            background-color: #4CAF;
-            color: white;
-        }
-
-        tr:nth-child(even) {
-            background-color: #f2f2f2;
-        }
-
-        tr:hover {
-            background-color: #ddd;
-        }
-
-        .button-container {
-            margin-top: 10px;
-        }
-
-        .button-container button {
-            margin-right: 5px;
-        }
-    </style>
+    <title>Penjualan</title>
+    <style>
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		th, td {
+			padding: 8px;
+			text-align: left;
+			border-bottom: 1px solid #ddd;
+		}
+		tr:hover {
+			background-color: #f5f5f5;
+		}
+		th {
+			background-color: #353A40;
+			color: white;
+		}
+	</style>
 </head>
 <body>
-  <h1>tampilan transaksi penjualan</h1>
-  <button onclick="location.href='create_produk.php'">Tambah Produk</button> <!-- Tambahkan tombol Tambah Produk -->
-
-  <?php
-  // Koneksi ke database
-  require 'koneksi.php';
-
-  // Query untuk mendapatkan data semua produk
-  $sql = "SELECT * FROM transaksi_penjualan";
-  $result = mysqli_query($conn, $sql);
-
-  // Memeriksa apakah ada produk yang ditemukan
-  if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    echo "<tr>
-        <th>id_transaksi_penjualan</th>
-        <th>penjualan_retail</th>
-        <th>penjualan_grosir</th>
-        <th>penjualan_aksesoris</th>
-        <th>pegawai_id_pegawai</th>
-        <th>pegawai_id_manajer</th>
-        <th>tanggal</th>
-
-        </tr>";
-    // Menampilkan data produk ke dalam tabel
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<tr>";
-      echo "<td>" . $row['id_transaksi_penjualan'] . "</td>";
-      echo "<td>" . $row['penjualan_retail'] . "</td>";
-      echo "<td>" . $row['penjualan_grosir'] . "</td>";
-      echo "<td>" . $row['penjualan_aksesoris'] . "</td>";
-      echo "<td>" . $row['pegawai_id_pegawai'] . "</td>";
-      echo "<td>" . $row['pegawai_id_manajer'] . "</td>";
-      echo "<td>" . $row['tanggal'] . "</td>";
-      echo "<td><a href='update_produk.php?id_transaksi_penjualan=" . $row['id_transaksi_penjualan'] . "'>edit</a> | ";
-      echo "<form method='POST' action='delete_produk.php'>";
-      echo "<input type='hidden' name='id_transaksi_penjualan' value='" . $row['id_transaksi_penjualan'] . "'>";
-      echo "<input type='submit' name='delete' value='Hapus'></form></td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-  } else {
-    echo "Tidak ada produk yang ditemukan.";
-  }
-
-  // Tutup koneksi database
-  mysqli_close($conn);
-  ?>
+    <h2>Transactions</h2>
+    <a href="">tambah_penjualan</a>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>penjualan retail</th>
+                <th>penjualan grosir</th>
+                <th>Pegawai ID</th>
+                <th>Mnajer ID</th>
+                <th>Tanggal</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            while ($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row['id_transaksi_penjualan'] . "</td>";
+                echo "<td>" . $row['penjualan_retail'] . "</td>";
+                echo "<td>" . $row['penjualan_grosir'] . "</td>";
+                echo "<td>" . $row['pegawai_id_pegawai'] . "</td>";
+                echo "<td>" . $row['pegawai_id_manajer'] . "</td>";
+                echo "<td>" . $row['tanggal'] . "</td>";
+                echo "<td>";
+                echo "<a href=\"edit_transaksi.php?id_transaksi=" . $row['id_transaksi_penjualan'] . "\">Edit</a>";
+                echo "<a href=\"delete_transaksi.php?id_transaksi=" . $row['id_transaksi_penjualan'] . "\" onclick=\"return confirm('Are you sure?');\">Delete</a>";
+                echo "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 </html>
+
+<?php
+$result->close();
+$conn->close();
+?>
