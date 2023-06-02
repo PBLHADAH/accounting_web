d50af5f8f851fe23c074c6a4d1e37b62de4b24ea
Maulana775 <anakbudiman01@gmail.com>
2023-06-02 14:30:00 +0700

    yang update hanif coy

--- Index Utama/index.php
+++ Index Utama/index.php
@@ -5,7 +5,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Website Layout | CodingLab</title>
+  <title>Aldy'z Cell</title>
   <link rel="stylesheet" href="styles.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" />
   <style>
@@ -190,6 +190,8 @@
       </div>
     </div>
   </div>
+
+  
 </body>
 
 </html>
