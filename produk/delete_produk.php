3d71baf5c737c050ff31fa92a69b7587e2951c61
exalth <rokindelosing@gmail.com>
2023-06-02 15:42:42 +0700

    produk alfin

---
+++ produk/delete_produk.php
@@ -0,0 +1,29 @@
+<?php
+if (isset($_POST['delete'])) {
+  $id_produk = $_POST['id_produk'];
+
+  // Koneksi ke database
+  require 'koneksi.php';
+
+  // Membuat koneksi ke database
+  $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+  // Memeriksa koneksi database
+  if (!$conn) {
+      die("Koneksi database gagal: " . mysqli_connect_error());
+  }
+
+  // Query untuk menghapus data produk berdasarkan ID
+  $sql = "DELETE FROM produk WHERE id_produk = '$id_produk'";
+  if (mysqli_query($conn, $sql)) {
+    // Penghapusan berhasil
+    header("Location: kelolaproduk.php"); // Redirect kembali ke halaman kelolaproduk.php
+  } else {
+    // Terjadi kesalahan dalam penghapusan data
+    echo "Gagal menghapus data: " . mysqli_error($conn);
+  }
+
+  // Tutup koneksi database
+  mysqli_close($conn);
+}
+?>
