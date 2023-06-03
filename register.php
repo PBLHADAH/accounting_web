eec26e3b8c0f51c3fa5e824bcd6291e3b1684847
exalth <rokindelosing@gmail.com>
2023-06-03 16:44:55 +0700

    hanif update tampilan register

--- register.php
+++ register.php
@@ -3,27 +3,91 @@
 <head>
     <title>Registration Page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <style>
+        body {
+            background-color: #f8f9fa;
+        }
+        
+        .container {
+            max-width: 500px;
+            margin: 100px auto;
+            padding: 20px;
+            background-color: #fff;
+            border-radius: 5px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        
+        h2 {
+            text-align: center;
+            margin-bottom: 30px;
+        }
+        
+        label {
+            font-weight: bold;
+        }
+        
+        input[type="text"],
+        input[type="password"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            border: 1px solid #ccc;
+            margin-bottom: 20px;
+        }
+        
+        input[type="submit"] {
+            width: 100%;
+            padding: 10px;
+            border-radius: 5px;
+            background-color: #007bff;
+            color: #fff;
+            font-weight: bold;
+            border: none;
+        }
+        
+        input[type="submit"]:hover {
+            background-color: #0069d9;
+            cursor: pointer;
+        }
+        
+        a {
+            display: block;
+            text-align: center;
+            margin-top: 20px;
+            color: #007bff;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
-    <h2>Register</h2>
-    <form method="POST" action="register.php">
-        <label>Nama Lengkap:</label>
-        <input type="text" name="nama_lengkap" required><br><br>
-        <label>No Hp:</label>
-        <input type="text" name="no_hp" required><br><br>
-        <label>Alamat:</label>
-        <input type="text" name="alamat" required><br><br>
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <label for="password">Konfirmasi password:</label>
-        <input type="password" name="konfirmasi" required><br><br>
-        <input type="submit" value="Register">
-    </form>
-    <a href="login.php">Login</a>
+    <div class="container">
+        <h2>Register</h2>
+        <form method="POST" action="register.php">
+            <label for="nama_lengkap">Nama Lengkap:</label>
+            <input type="text" id="nama_lengkap" name="nama_lengkap" required>
+            
+            <label for="no_hp">No Hp:</label>
+            <input type="text" id="no_hp" name="no_hp" required>
+            
+            <label for="alamat">Alamat:</label>
+            <input type="text" id="alamat" name="alamat" required>
+            
+            <label for="username">Username:</label>
+            <input type="text" id="username" name="username" required>
+            
+            <label for="password">Password:</label>
+            <input type="password" id="password" name="password" required>
+            
+            <label for="konfirmasi">Konfirmasi password:</label>
+            <input type="password" id="konfirmasi" name="konfirmasi" required>
+            
+            <input type="submit" value="Register">
+        </form>
+        <a href="login.php">Login</a>
+    </div>
 </body>
 </html>
+
 <?php
 // Establish a connection to the database
 require 'koneksi.php';
