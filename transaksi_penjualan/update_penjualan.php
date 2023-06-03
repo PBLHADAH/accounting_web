eb03e9d763a1024b6f0a73e16af8102a59824cdb
Holil9 <mohholilagil91643@gmail.com>
2023-06-03 15:25:28 +0700

    uuu

--- transaksi_penjualan/update_penjualan.php
+++ transaksi_penjualan/update_penjualan.php
@@ -1,70 +1,144 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Edit Transaction</title>
+    <title>tambah penjualan.</title>
+    <style>
+        /* Style untuk button */
+        button {
+            padding: 10px 20px;
+            background-color: #2268EF;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        button:hover {
+            background-color: #1d54bf;
+        }
+
+        /* Style untuk input */
+        input[type="submit"] {
+            padding: 10px 20px;
+            background-color: #2268EF;
+            color: white;
+            border: none;
+            border-radius: 4px;
+            cursor: pointer;
+        }
+
+        input[type="submit"]:hover {
+            background-color: #1d54bf;
+        }
+    </style>
 </head>
 <body>
-    <h2>Edit Transaction</h2>
-    <?php
-    require_once "../koneksi.php";
-
-    function sanitize($input)
-    {
-        global $conn;
-        $output = mysqli_real_escape_string($conn, $input);
-        $output = htmlspecialchars($output);
-        return $output;
-    }
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        $id_transaksi_penjualan = sanitize($_POST["id_transaksi_penjualan"]);
-        $penjualan_retail = sanitize($_POST["penjualan_retail"]);
-        $penjualan_aksesoris = sanitize($_POST["penjualan_aksesoris"]);
-        $pegawai_id_pegawai = sanitize($_POST["pegawai_id_pegawai"]);
-        $pegawai_id_manajer = sanitize($_POST["pegawai_id_pegawai"]);
-        
-
-
-        $sql = "UPDATE transaksi_lainnya SET penjualan_retail='$penjualan_retail', penjualan_aksesoris='$penjualan_aksesoris', pegawai_id_pegawai_pegawai='$pegawai_id_pegawai', WHERE id_transaksi_penjualan_lainnya='$id_transaksi_penjualan'";
-
-        if ($conn->query($sql) === TRUE) {
-            header('Location: transaksi_lainnya.php'); // ganti
-        } else {
-            echo "Error updating transaction: " . $conn->error;
-        }
-    } else {
-        $id_transaksi_penjualan = $_GET['id_transaksi_penjualan'];
-
-        $sql = "SELECT * FROM transaksi_lainnya WHERE id_transaksi_penjualan_lainnya='$id_transaksi_penjualan'";
-        $result = $conn->query($sql);
-        $row = $result->fetch_assoc();
-    ?>
-
-    <form action="edit_transaksi.php" method="post">
-        <input type="hidden" name="id_transaksi_penjualan" value="<?php echo $row['id_transaksi_penjualan_lainnya']; ?>">
-        <label for="penjualan_retail">penjualan_retail:</label><br>
-        <input type="text" name="penjualan_retail" value="<?php echo $row['penjualan_retail']; ?>"><br><br>
-        <label for="penjualan_aksesoris">penjualan_aksesoris:</label><br>
-        <input type="number" name="penjualan_aksesoris" value="<?php echo $row['penjualan_aksesoris']; ?>"><br><br>
-        <label for="pegawai_id_pegawai">Pegawai ID:</label><br>
+    <h1>Transaksi Penjualan</h1>
+    <form action="" method="POST">
+        <label for="penjualan_retail">Retail :</label>
+        <input type="text" id="penjualan_retail" name="penjualan_retail" required><br><br>
+        <label for="penjualan_grosir">Grosir :</label>
+        <input type="text" id="penjualan_grosir" name="penjualan_grosir" required><br><br>
+        <label for="penjualan_aksesoris">Aksesoris :</label>
+        <input type="text" id="penjualan_aksesoris" name="penjualan_aksesoris" required><br><br>
+        <label for="pegawai_id_pegawai">Pegawai :</label>
         <select id="pegawai_id_pegawai" name="pegawai_id_pegawai" required>
-        <?php
-        $sql = "SELECT id_pegawai, nama FROM pegawai";
-        $result = mysqli_query($conn, $sql);
+            <option value="">Pilih pegawai</option>
+            <?php
+            // Koneksi ke database
+            require 'koneksi.php';
 
-        while ($pegawai = mysqli_fetch_assoc($result)) {
-            $selected = ($pegawai['id_pegawai'] == $row['pegawai_id_pegawai_pegawai']) ? "selected" : "";
-            echo "<option value='" . $pegawai['id_pegawai'] . "' " . $selected . ">" . $pegawai['nama'] . "</option>";
-        }
-        ?>
+            // Membuat koneksi ke database
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            // Memeriksa koneksi database
+            if (!$conn) {
+                die("Koneksi database gagal: " . mysqli_connect_error());
+            }
+
+            // Query untuk mengambil data pegawai dengan jabatan "pegawai" dari tabel
+            $sql = "SELECT id_pegawai, nama FROM pegawai WHERE jabatan = 'pegawai'";
+            $result = mysqli_query($conn, $sql);
+
+            // Menghasilkan opsi dropdown berdasarkan data pegawai dengan jabatan "pegawai"
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<option value='" . $row['id_pegawai'] . "'>" . $row['nama'] . "</option>";
+            }
+
+            // Tutup koneksi database
+            mysqli_close($conn);
+            ?>
         </select><br><br>
-        <input type="submit" value="Update">
+        
+        <label for="pegawai_id_manajer">Manajer :</label>
+        <select id="pegawai_id_manajer" name="pegawai_id_manajer" required>
+            <option value="">Pilih manajer</option>
+            <?php
+            // Membuat koneksi ke database
+            $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+            // Memeriksa koneksi database
+            if (!$conn) {
+                die("Koneksi database gagal: " . mysqli_connect_error());
+            }
+
+            // Query untuk mengambil data pegawai dengan jabatan "manajer" dari tabel
+            $sql = "SELECT id_pegawai, nama FROM pegawai WHERE jabatan = 'manajer'";
+            $result = mysqli_query($conn, $sql);
+
+            // Menghasilkan opsi dropdown berdasarkan data pegawai dengan jabatan "manajer"
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<option value='" . $row['id_pegawai'] . "'>" . $row['nama'] . "</option>";
+            }
+
+            // Tutup koneksi database
+            mysqli_close($conn);
+            ?>
+        </select><br><br>
+
+        <input type="hidden" id="tanggal" name="tanggal" value="<?php echo date('Y-m-d'); ?>">
+
+        <button onclick="location.href='Table_penjualan.php'">Kembali</button>
+        <input type="submit" name="submit" value="Konfirmasi">
     </form>
-
-    <?php
-    }
-
-    $conn->close();
-    ?>
 </body>
 </html>
+
+<?php
+if (isset($_POST['submit'])) {
+    $penjualan_retail = $_POST['penjualan_retail'];
+    $penjualan_grosir = $_POST['penjualan_grosir'];
+    $penjualan_aksesoris = $_POST['penjualan_aksesoris'];
+    $pegawai_id_pegawai = $_POST['pegawai_id_pegawai'];
+    $pegawai_id_manajer = $_POST['pegawai_id_manajer'];
+    $tanggal = $_POST['tanggal'];
+
+    // Koneksi ke database
+    require 'koneksi.php';
+
+    // Membuat koneksi ke database
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Memeriksa koneksi database
+    if (!$conn) {
+        die("Koneksi database gagal: " . mysqli_connect_error());
+    }
+
+    // Query untuk menyimpan data produk ke tabel
+    // Query untuk menyimpan data produk ke tabel
+// Query untuk menyimpan data produk ke tabel
+$sql = "INSERT INTO transaksi_penjualan (penjualan_retail, penjualan_grosir, penjualan_aksesoris, pegawai_id_pegawai, pegawai_id_manajer, tanggal) VALUES ('$penjualan_retail', '$penjualan_grosir', '$penjualan_aksesoris', '$pegawai_id_pegawai', '$pegawai_id_manajer', '$tanggal')";
+
+    if (mysqli_query($conn, $sql)) {
+        // Penyimpanan berhasil
+        header("Location: Table_penjualan.php"); // Redirect kembali ke halaman kelolaproduk.php
+        exit();
+    } else {
+        // Terjadi kesalahan dalam penyimpanan data
+        echo "Gagal menyimpan data: " . mysqli_error($conn);
+    }
+
+    // Tutup koneksi database
+    mysqli_close($conn);
+}
+?>
