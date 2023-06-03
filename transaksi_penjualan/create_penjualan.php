a959711d1d5aa7639b62a794ccbbefab15d4c9ca
Maulana775 <anakbudiman01@gmail.com>
2023-06-03 10:27:35 +0700

    maulana

--- transaksi_penjualan/create_penjualan.php
+++ transaksi_penjualan/create_penjualan.php
@@ -1,10 +1,6 @@
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tambah penjualan</title>
     <style>
         /* Style untuk button */
@@ -38,14 +34,13 @@
 </head>
 <body>
     <h1>Transaksi Penjualan</h1>
-    <form method="POST" action="penjualan.php">
+    <form action="" method="POST">
         <label for="penjualan_retail">Retail :</label>
         <input type="text" id="penjualan_retail" name="penjualan_retail" required><br><br>
         <label for="penjualan_grosir">Grosir :</label>
         <input type="text" id="penjualan_grosir" name="penjualan_grosir" required><br><br>
         <label for="penjualan_aksesoris">Aksesoris :</label>
         <input type="text" id="penjualan_aksesoris" name="penjualan_aksesoris" required><br><br>
-        
         <label for="pegawai_id_pegawai">Pegawai :</label>
         <select id="pegawai_id_pegawai" name="pegawai_id_pegawai" required>
             <option value="">Pilih pegawai</option>
@@ -62,12 +57,12 @@
             }
 
             // Query untuk mengambil data pegawai dengan jabatan "pegawai" dari tabel
-            $sql = "SELECT nama FROM pegawai WHERE jabatan = 'pegawai'";
+            $sql = "SELECT id_pegawai, nama FROM pegawai WHERE jabatan = 'pegawai'";
             $result = mysqli_query($conn, $sql);
 
             // Menghasilkan opsi dropdown berdasarkan data pegawai dengan jabatan "pegawai"
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "<option value='" . $row['nama'] . "'>" . $row['nama'] . "</option>";
+                echo "<option value='" . $row['id_pegawai'] . "'>" . $row['nama'] . "</option>";
             }
 
             // Tutup koneksi database
@@ -88,12 +83,12 @@
             }
 
             // Query untuk mengambil data pegawai dengan jabatan "manajer" dari tabel
-            $sql = "SELECT nama FROM pegawai WHERE jabatan = 'manajer'";
+            $sql = "SELECT id_pegawai, nama FROM pegawai WHERE jabatan = 'manajer'";
             $result = mysqli_query($conn, $sql);
 
             // Menghasilkan opsi dropdown berdasarkan data pegawai dengan jabatan "manajer"
             while ($row = mysqli_fetch_assoc($result)) {
-                echo "<option value='" . $row['nama'] . "'>" . $row['nama'] . "</option>";
+                echo "<option value='" . $row['id_pegawai'] . "'>" . $row['nama'] . "</option>";
             }
 
             // Tutup koneksi database
@@ -104,34 +99,46 @@
         <input type="hidden" id="tanggal" name="tanggal" value="<?php echo date('Y-m-d'); ?>">
 
         <button onclick="location.href='penjualan.php'">Kembali</button>
-        <input type="submit" name="konfirmasi" value="Konfirmasi">
+        <input type="submit" name="submit" value="Konfirmasi">
     </form>
 </body>
 </html>
+
 <?php
-if (isset($_POST['konfirmasi'])) {
-$penjualan_retail = $_POST['penjualan_retail'];
-$penjualan_grosir = $_POST['penjualan_grosir'];
-$penjualan_aksesoris = $_POST['penjualan_aksesoris'];
-$pegawai_id_pegawai = $_POST['pegawai_id_pegawai'];
-$pegawai_id_manajer = $_POST['pegawai_id_manajer'];
-$tanggal = $_POST['tanggal'];
+if (isset($_POST['submit'])) {
+    $penjualan_retail = $_POST['penjualan_retail'];
+    $penjualan_grosir = $_POST['penjualan_grosir'];
+    $penjualan_aksesoris = $_POST['penjualan_aksesoris'];
+    $pegawai_id_pegawai = $_POST['pegawai_id_pegawai'];
+    $pegawai_id_manajer = $_POST['pegawai_id_manajer'];
+    $tanggal = $_POST['tanggal'];
 
-// Koneksi ke database
-require 'koneksi.php';
+    // Koneksi ke database
+    require 'koneksi.php';
 
+    // Membuat koneksi ke database
+    $conn = mysqli_connect($servername, $username, $password, $dbname);
+
+    // Memeriksa koneksi database
+    if (!$conn) {
+        die("Koneksi database gagal: " . mysqli_connect_error());
+    }
+
+    // Query untuk menyimpan data produk ke tabel
+    // Query untuk menyimpan data produk ke tabel
 // Query untuk menyimpan data produk ke tabel
 $sql = "INSERT INTO transaksi_penjualan (penjualan_retail, penjualan_grosir, penjualan_aksesoris, pegawai_id_pegawai, pegawai_id_manajer, tanggal) VALUES ('$penjualan_retail', '$penjualan_grosir', '$penjualan_aksesoris', '$pegawai_id_pegawai', '$pegawai_id_manajer', '$tanggal')";
-if (mysqli_query($conn, $sql)) {
-    // Penyimpanan berhasil
-    header("Location: kelolaproduk.php"); // Redirect kembali ke halaman kelolaproduk.php
-    exit();
-} else {
-    // Terjadi kesalahan dalam penyimpanan data
-    echo "Gagal menyimpan data: " . mysqli_error($conn);
-}
 
-// Tutup koneksi database
-mysqli_close($conn);
+    if (mysqli_query($conn, $sql)) {
+        // Penyimpanan berhasil
+        header("Location: penjualan.php"); // Redirect kembali ke halaman kelolaproduk.php
+        exit();
+    } else {
+        // Terjadi kesalahan dalam penyimpanan data
+        echo "Gagal menyimpan data: " . mysqli_error($conn);
+    }
+
+    // Tutup koneksi database
+    mysqli_close($conn);
 }
 ?>
