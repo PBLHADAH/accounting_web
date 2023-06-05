bc7322a126f0d47c41ecc57239aca7b5960bbe0f
exalth <rokindelosing@gmail.com>
2023-06-05 14:33:35 +0700

    asdfg

--- produk/produk.php
+++ produk/produk.php
@@ -6,39 +6,44 @@
 <body>
   <h1>Kelola Produk</h1>
   <button onclick="location.href='produk/create_produk.php'">Tambah Produk</button> <!-- Tambahkan tombol Tambah Produk -->
+  <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nama Supplier</th>
+                <th>Alamat</th>
+                <th>No. Telepon</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+        <?php
+        // Koneksi ke database
+        require 'koneksi.php';
 
-  <?php
-  // Koneksi ke database
-  require 'koneksi.php';
+        // Query untuk mendapatkan data semua produk
+        $sql = "SELECT * FROM produk";
+        $result = mysqli_query($conn, $sql);
+        
+        // Memeriksa apakah ada produk yang ditemukan
+        if (mysqli_num_rows($result) > 0) {
+          
+          // Menampilkan data produk ke dalam tabel
+          while ($row = mysqli_fetch_assoc($result)) {
+            echo "<tr>";
+            echo "<td>" . $row['id_produk'] . "</td>";
+            echo "<td>" . $row['nama_produk'] . "</td>";
+            echo "<td> Rp." . number_format($row['harga_produk'], 0, ',', '.') . "</td>";
+            echo "<td><a href='produk/update_produk.php?id_produk=" . $row['id_produk'] . "'>Edit</a> | ";
+            echo "<form method='POST' action='produk/delete_produk.php'>";
+            echo "<input type='hidden' name='id_produk' value='" . $row['id_produk'] . "'>";
+            echo "<input type='submit' name='delete' value='Hapus'></form></td>";
+            echo "</tr>";
+          }
 
-  // Query untuk mendapatkan data semua produk
-  $sql = "SELECT * FROM produk";
-  $result = mysqli_query($conn, $sql);
-
-  // Memeriksa apakah ada produk yang ditemukan
-  if (mysqli_num_rows($result) > 0) {
-    echo "<table>";
-    echo "<tr><th>ID Produk</th><th>Nama Produk</th><th>Harga Produk</th><th>Aksi</th></tr>";
-    // Menampilkan data produk ke dalam tabel
-    while ($row = mysqli_fetch_assoc($result)) {
-      echo "<tr>";
-      echo "<td>" . $row['id_produk'] . "</td>";
-      echo "<td>" . $row['nama_produk'] . "</td>";
-      echo "<td> Rp." . number_format($row['harga_produk'], 0, ',', '.') . "</td>";
-      echo "<td><a href='produk/update_produk.php?id_produk=" . $row['id_produk'] . "'>Edit</a> | ";
-      echo "<form method='POST' action='produk/delete_produk.php'>";
-      echo "<input type='hidden' name='id_produk' value='" . $row['id_produk'] . "'>";
-      echo "<input type='submit' name='delete' value='Hapus'></form></td>";
-      echo "</tr>";
-    }
-    echo "</table>";
-  } else {
-    echo "Tidak ada produk yang ditemukan.";
-  }
-
-  // Tutup koneksi database
-  mysqli_close($conn);
+        } 
   ?>
+</tbody>
+    </table>
 </body>
-</html>
-                   
+</html>             
